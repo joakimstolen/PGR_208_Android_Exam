32aa4520b456f64e1d29db8abb158d5e063a0e72
joakimstolen <joakimstoelen@hotmail.com>
2020-04-13 12:04:46 +0200

    Minor refactoring

--- app/src/main/java/com/example/exam_testing/Activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/exam_testing/Activity/SplashScreenActivity.kt
@@ -1,10 +1,10 @@
-package com.example.exam_testing
+package com.example.exam_testing.Activity
 
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.example.exam_testing.Activity.MainActivity
+import com.example.exam_testing.R
 
 class SplashScreenActivity : AppCompatActivity() {
 
